17e044a4bc1ad96874198f3a45d92672cc7c673d
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-16 01:24:02 -0300

    feat: add fonts and update title and meta tag

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,7 +16,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Poppins', 'Roboto', 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
 
