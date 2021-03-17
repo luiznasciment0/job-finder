cac7dae51f74edd98f70ebf29aa108cf644d1b22
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-17 00:22:23 -0300

    feat #1: start creating theme provider

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+export default {
+  colors: {
+    mainBg: '#F6F7FB',
+    black: '#282538',
+    palegray: '#B9BDCF',
+    white: '#FFFFFF',
+    blue: '#1E86FF',
+    darkBlue: '#334680'
+  },
+  font: {
+    family: {
+      primary: `font-family: 'Poppins', sans-serif`,
+      secondary: `font-family: 'Roboto', sans-serif`,
+      tertiary: `font-family: 'Montserrat', sans-serif`
+    },
+    light: 300,
+    normal: 400,
+    medium: 500,
+    bold: 700,
+    sizes: {
+      small: '1.2rem',
+      medium: '1.4rem',
+      large: '1.6rem',
+      xlarge: '2.4rem'
+    }
+  }
+} as const
