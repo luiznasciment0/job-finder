9c0d9934eed7631c1ca2ee93d4e19fcb7fb15238
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-18 02:50:53 -0300

    feat: add default css beautify

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,7 +4,11 @@ const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   ${({ theme }) => css`
