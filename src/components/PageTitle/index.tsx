974cda7190b5109420e15d5e5c6450e99b7fa98b
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-21 18:41:04 -0300

    feat: create page title component

---
+++ src/components/PageTitle/index.tsx
@@ -0,0 +1,9 @@
+import Typography from 'components/Typography'
+
+const PageTitle = () => (
+  <Typography size="xlarge" color="black">
+    <strong>Github</strong> Jobs
+  </Typography>
+)
+
+export default PageTitle
