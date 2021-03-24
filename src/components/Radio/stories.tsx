6ec0de92ea156f2497b98162ef3d9a62360bc539
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-24 00:20:16 -0300

    fix radio component

--- src/components/Radio/stories.tsx
+++ src/components/Radio/stories.tsx
@@ -1,9 +1,9 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Item from '.'
+import Item from './index'
 
 export default {
   title: 'Item',
   component: Item
 } as Meta
 
-export const Default: Story = () => <Item item="Teste" />
+export const Default: Story = () => <Item label="Test" />
