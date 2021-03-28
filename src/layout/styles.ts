aaa9d0f1d2740f09f4e1256a763d03f70248d37b
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-28 15:28:03 -0300

    create static layout

---
+++ src/layout/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Content = styled.div`
+  padding: 1.2rem;
+  width: fit-content;
+  header {
+    margin-bottom: 3.2rem;
+  }
+`
+
+export const Main = styled.main`
+  /* padding: 1.2rem; */
+`
+
+export const CheckboxWrapper = styled.div`
+  margin: 2.9rem 1.4rem 3.5rem;
+`
