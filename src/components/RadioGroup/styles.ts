ec5148887159d6f6b496a1bd74186ddf9b3d3d98
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-28 15:27:41 -0300

    create radiogroup

---
+++ src/components/RadioGroup/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.867rem;
+  margin-left: 1.4rem;
+  margin-bottom: 2.6rem;
+`
